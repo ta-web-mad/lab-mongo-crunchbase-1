ca1fcf03946285eed9397604d62cd54349a6a960
MLuisa Santos <mls_mansell@outlook.com>
2020-11-09 22:21:49 +0100

    done

--- starter-code/main.js
+++ starter-code/main.js
@@ -57,6 +57,95 @@ mongoClient.connect(url, (error, db) => {
                 }
               })
             break;
+          case "4":
+            //List by name all companies founded in february of 2004.
+            db.collection('companies').find({ $and: [{ "founded_year": 2004 }, { "founded_month": 2 }] }, {name: 1, _id: 0}).toArray((error, result) => {
+                if (error) {
+                  console.log(error);
+                  rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+                } else {
+                  console.log(result);
+                  rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+                }
+              })
+            break;
+          case "5":
+            //List by name all companies founded in the summer of 2004 (april to june) sorted by date.
+            db.collection('companies').find({$and:[{"founded_year":2004},{"founded_month":{$gte:4}},{"founded_month":{$lte:6}}]}  , {name: 1, _id: 0}).toArray((error, result) => {
+                if (error) {
+                  console.log(error);
+                  rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+                } else {
+                  console.log(result);
+                  rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+                }
+              })
+            break;
+           case "6":
+            //What companies have offices in "Barcelona".
+            db.collection('companies').find({"offices.city":"Barcelona"}  , {name: 1, _id: 0}).toArray((error, result) => {
+                if (error) {
+                  console.log(error);
+                  rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+                } else {
+                  console.log(result);
+                  rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+                }
+              })
+            break;
+          case "7":
+            //List the 10 companies with more employees sorted ascending (show name and employees).
+            db.collection('companies').find({"number_of_employees": {$gt: 100000}} , {name: 1, _id: 0}).sort({"number_of_employees":1}).toArray((error, result) => {
+                if (error) {
+                  console.log(error);
+                  rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+                } else {
+                  console.log(result);
+                  rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+                }
+              })
+            break;
+          case "8":
+            //Find the company with the name "Facebook"
+            db.collection('companies').find({"name":"Facebook"},{name: 1, _id: 0}).toArray((error, result) => {
+                if (error) {
+                  console.log(error);
+                  rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+                } else {
+                  console.log(result);
+                  rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+                }
+              })
+            break;
+          case "9":
+            //How many employees has Facebook?
+            db.collection('companies').find({"name":"Facebook"},{number_of_employees: 1, _id: 0}).toArray((error, result) => {
+                if (error) {
+                  console.log(error);
+                  rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+                } else {
+                  console.log(result);
+                  rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+                }
+              })
+            break;
+           case "10":
+            //List the name of all the products of Facebook
+            
+            //No sabemos como sacar los productos de FB
+            db.collection('companies').find({"name":"Facebook"},{number_of_employees: 1, _id: 0}).toArray((error, result) => {
+                if (error) {
+                  console.log(error);
+                  rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+                } else {
+                  console.log(result);
+                  rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+                }
+              })
+            break;
+
+
+
         }
       });
       }
