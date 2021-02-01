9fe635bb0325ee183faf8c2d2e21c1c773075953
montse <montserrat.mosqueda@gmail.com>
2021-02-01 22:19:51 +0100

    COMMIT

--- starter-code/main.js
+++ starter-code/main.js
@@ -58,79 +58,153 @@ mongoClient.connect(`mongodb://localhost:27017/crunchbase`, (error, db) => {
 
 
           case "4":
-          // 4.- List by name all companies founded in february of 2004.
-
+            // 4.- List by name all companies founded in february of 2004.
+            db.collection('companies').find({ $and: [{ founded_year: 2004 }, { founded_month: 2 }] }, { name: 1, _id: 0 }).toArray((error, result) => {
+              if (error) {
+                console.log(error);
+                rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+              } else {
+                console.log(result);
+                rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+              }
+            })
+            break;
 
 
           case "5":
-          // 5.- List by name all companies founded in the summer of 2004 (april to june) sorted by date.
-
+            // 5.- List by name all companies founded in the summer of 2004 (april to june) sorted by date.
+            db.collection('companies').find({ $and: [{ founded_year: 2004 }, { founded_month: { $gte: 4 } }, { founded_month: { $lte: 6 } }] }, { name: 1, founded_year: 1, founded_month: 1, founded_day: 1, _id: 0 }).sort(
+              { founded_year: 1, founded_month: 1, founded_day: 1 }).toArray((error, result) => {
+                if (error) {
+                  console.log(error);
+                  rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+                } else {
+                  console.log(result);
+                  rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+                }
+              })
+            break;
 
 
           case "6":
-          // 6.- What companies have offices in "Barcelona".
-
+            // 6.- What companies have offices in "Barcelona".
+            db.collection('companies').find({ "offices.city": { $regex: /barcelona/, $options: 'i' } }, { name: 1, _id: 0 }).toArray((error, result) => {
+              if (error) {
+                console.log(error);
+                rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+              } else {
+                console.log(result);
+                rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+              }
+            })
+            break;
 
 
           case "7":
-          // 7.- List the 10 companies with more employees sorted ascending (show name and employees).
-
+            // 7.- List the 10 companies with more employees sorted ascending (show name and employees).
+            db.collection('companies').find({}, { more_employees: 1, name: 1, _id: 0 }).sort({ more_employees: -1 }).limit(10).toArray((error, result) => {
+              if (error) {
+                console.log(error);
+                rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+              } else {
+                console.log(result);
+                rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+              }
+            })
+            break;
 
 
           case "8":
-          // 8.- Find the company with the name "Facebook"
-
-
+            // 8.- Find the company with the name "Facebook"
+            db.collection('companies').find({ name: 'Facebook' }).toArray((error, result) => {
+              if (error) {
+                console.log(error);
+                rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+              } else {
+                console.log(result);
+                rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+              }
+            })
+            break;
 
 
           case "9":
-          // 9.- How many employees has Facebook?
-
+            // 9.- How many employees has Facebook?
+            db.collection('companies').find({ name: 'Facebook' }, { number_of_employees: 1, _id: 0 }).toArray((error, result) => {
+              if (error) {
+                console.log(error);
+                rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+              } else {
+                console.log(result);
+                rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+              }
+            })
+            break;
 
           case "10":
-          // 10.- List the name of all the products of Facebook
-
+            // 10.- List the name of all the products of Facebook
+            db.collection('companies').find({ name: 'Facebook' }, { 'products.name': 1, _id: 0 }).toArray((error, result) => {
+              if (error) {
+                console.log(error);
+                rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+              } else {
+                console.log(result[0]);
+                rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+              }
+            })
+            break;
 
           case "11":
-          // 11.- List the people that are working at Facebook right now (check relationships field)
-
+            // 11.- List the people that are working at Facebook right now (check relationships field)
+            // ***** NO SUPE COMO ACCEDER AL OBJETO*****
+            db.collection('companies').find({ $and: [{ name: 'Facebook' }, { 'relationships.is_past': false }] }, { 'relationships.person': 1, _id: 0 }).toArray((error, result) => {
+              if (error) {
+                console.log(error);
+                rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+              } else {
+                console.log(result[0]);
+                rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+              }
+            })
+            break;
 
 
           case "12":
-          // 12.- List all the companies where "david-ebersman" has worked.
+            // 12.- List all the companies where "david-ebersman" has worked.
+            db.collection('companies').find({ 'relationships.person.permalink': 'david-ebersman' }, { name: 1, _id: 0 }).toArray((error, result) => {
+              if (error) {
+                console.log(error);
+                rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+              } else {
+                console.log(result);
+                rl.question(`\nType enter to continue: `, (answer) => { mainMenu() });
+              }
+            })
+            break;
 
 
-          case "13":
-          // 13.- List by name the competitors of Facebook
-
-
-
-          case "14":
-          // 14.- Names of the companies that has "social-networking" in tag-list (be aware that the value of field is a string check regex operators)
-
-
-
-          case "15":
-          // 15.- How many companies that has "social-network" in tag-list and founded between 2002 and 2016 inclusive
-
-
-
-          case "16":
-          // 16.- Names and locations of companies that have offices in London
+          // case "13":
+          // // 13.- List by name the competitors of Facebook
 
 
 
-          case "17":
-          // 17.- How many companies that has "social-network" in tag-list and founded between 2002 and 2016 inclusive and has offices in New York
+          // case "14":
+          // // 14.- Names of the companies that has "social-networking" in tag-list (be aware that the value of field is a string check regex operators)
 
 
 
+          // case "15":
+          // // 15.- How many companies that has "social-network" in tag-list and founded between 2002 and 2016 inclusive
 
 
 
+          // case "16":
+          // // 16.- Names and locations of companies that have offices in London
 
 
 
+          // case "17":
+          // // 17.- How many companies that has "social-network" in tag-list and founded between 2002 and 2016 inclusive and has offices in New York
 
         }
       });
